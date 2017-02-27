91b63cf9fd3adc6b688fea668379a3acac87e8cb
darkgel <1627116986@qq.com>
2017-02-27 14:59:28 +0800

    测试连接微信服务器

---
+++ controllers/WeChatController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael.shi
+ * Date: 2017/2/27
+ * Time: 14:41
+ */
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class WeChatController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    public function actionCheckSignature()
+    {
+        echo "iamdarkgel";exit;
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
