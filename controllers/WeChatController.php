bd9d3d3a50c05d08a42a66eb21937cbb4175c72f
darkgel <1627116986@qq.com>
2017-03-01 15:37:58 +0800

    继续测试!!!!

--- controllers/WeChatController.php
+++ controllers/WeChatController.php
@@ -66,7 +66,7 @@ class WeChatController extends Controller
 /*        $input = file_get_contents("php://input"); //接收POST数据
         $xml = simplexml_load_string($input); //提取POST数据为simplexml对象
         Yii::$app->cache->set("xml",$xml);*/
-        $xml = Yii::$app->cache->set("xml");
+        $xml = Yii::$app->cache->get("xml");
         echo "<pre>";
         var_dump($xml);
         echo "</pre>";
